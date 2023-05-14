fb130ef421a78e5029949a5a86325c82caf59fa4
Dinh Thi Thu Hien <hien25042001@gmail.com>
2023-05-14 11:48:37 +0200

    up

--- app/index.py
+++ app/index.py
@@ -7,65 +7,66 @@ from flask_login import login_user, logout_user, current_user
 from flask_login import login_required
 from app.models import UserRole
 
-@app.route("/")
-def home(): 
 
+@app.route("/")
+def home():
     return render_template('index.html')
+
 
 @app.route("/about")
 def about():
-
     return render_template('about.html')
+
 
 @app.route("/blog")
 def blog():
-
     return render_template('blog.html')
+
 
 @app.route("/blog-single-1")
 def blog_single_1():
-
     return render_template('blog-single-1.html')
+
 
 @app.route("/blog-single-2")
 def blog_single_2():
-
     return render_template('blog-single-2.html')
+
 
 @app.route("/blog-single-3")
 def blog_single_3():
-
     return render_template('blog-single-3.html')
+
 
 @app.route("/blog-single-4")
 def blog_single_4():
-
     return render_template('blog-single-4.html')
+
 
 @app.route("/blog-single-5")
 def blog_single_5():
-
     return render_template('blog-single-5.html')
+
 
 @app.route("/blog-single-6")
 def blog_single_6():
-
     return render_template('blog-single-6.html')
+
 
 @app.route("/services")
 def services():
-
     return render_template('services.html')
+
 
 @app.route("/doctors")
 def doctors():
-
     return render_template('doctors.html')
+
 
 @app.route("/contact")
 def contact():
-
     return render_template('contact.html')
+
 
 @app.route("/fhir/Patient")
 @login_required
@@ -82,7 +83,8 @@ def patient_list():
                                   name=name,
                                   gender=gender)
 
-    return render_template('patient.html', patients=patients) 
+    return render_template('patient.html', patients=patients)
+
 
 @app.route("/fhir/Patient/<int:patient_id>")
 @login_required
@@ -92,10 +94,18 @@ def patient_detail(patient_id):
     return render_template('patient_detail.html',
                            patient=patient)
 
-@app.route("/fhir/Patient/add")
+
+@app.route("/fhir/Patient/add", methods=['get', 'post'])
 @login_required
 def add_or_update_patient():
-    return render_template("patient_add.html")
+    if request.method.lower() == "post":
+        return redirect(url_for('patient_list'))
+
+    return render_template("patient_add.html",
+                           immunization=utils.load_immunization(),
+                           practitioner=utils.load_practitioner(),
+                           observation=utils.load_observation())
+
 
 @app.route("/fhir/Immunization/<int:immunization_id>")
 @login_required
@@ -105,6 +115,7 @@ def immunization_detail(immunization_id):
     return render_template('immunization_detail.html',
                            immunization=immunization)
 
+
 @app.route("/fhir/Observation/<int:observation_id>")
 @login_required
 def observation_detail(observation_id):
@@ -113,6 +124,7 @@ def observation_detail(observation_id):
     return render_template('observation_detail.html',
                            observation=observation)
 
+
 @app.route("/fhir/Practitioner/<int:practitioner_id>")
 @login_required
 def practitioner_detail(practitioner_id):
@@ -120,6 +132,7 @@ def practitioner_detail(practitioner_id):
 
     return render_template('practitioner_detail.html',
                            practitioner=practitioner)
+
 
 @app.route('/fhir/_history')
 @login_required
@@ -134,6 +147,7 @@ def history():
                            immunizations=immunizations,
                            observations=observations,
                            practitioners=practitioners)
+
 
 @app.route('/register', methods=['get', 'post'])
 def user_register():
@@ -167,6 +181,7 @@ def user_register():
     return render_template('register.html',
                            err_msg=err_msg)
 
+
 @app.route('/user-login', methods=['get', 'post'])
 def user_signin():
     err_msg = ""
@@ -186,14 +201,17 @@ def user_signin():
     return render_template('login.html',
                            err_msg=err_msg)
 
+
 @app.route('/user-logout')
 def user_signout():
     logout_user()
     return redirect(url_for('user_signin'))
 
+
 @login.user_loader
 def user_load(user_id):
     return utils.get_user_by_id(user_id=user_id)
+
 
 @app.route('/info-perso')
 @login_required
