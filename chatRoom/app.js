12cbe4a96d1e9c88f8ae7cb927426e3a80983792
Dinh Thi Thu Hien <hien25042001@gmail.com>
2023-06-27 11:05:47 +0200

    up

---
+++ chatRoom/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname,'./templates')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/templates/chats.html');
+});
+
+
+var name;
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  
+  socket.on('joining msg', (username) => {
+  	name = username;
+  	io.emit('chat message', `❤️❤️❤️${name} joined the chat❤️❤️❤️`);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('chat message', `❤️❤️❤️${name} left the chat❤️❤️❤️`);
+    
+  });
+  socket.on('chat message', (msg) => {
+    //sending message to all except the sender
+    socket.broadcast.emit('chat message', msg);         
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on :3000');
+});
+
+
